735cc614d5561b9efe30c397972f9ca00203a4d3
semmelsamu <semmikroiss@gmail.com>
2023-01-04 22:49:27 +0100

    Finished basic children calculator

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-import { Plugin, Notice } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 export default class ancestry extends Plugin 
 {
-	ancestry: string[];
+	parents : any[string];
 	
 	async onload() 
 	{
@@ -10,36 +10,91 @@ export default class ancestry extends Plugin
 		
 		this.addRibbonIcon("reset", "Recalculate ancestry", async () => 
 		{
-			new Notice("Recalculating ancestry")
 			this.recalculateAncestry();
 		});
 		
+		this.recalculateAncestry();
+		
 		this.registerMarkdownCodeBlockProcessor("ancestry", (source, el, ctx) => 
 		{
-			el.createEl("p", { text: "Hello" });
+			let currentPerson = source.trim();
+			
+			let result: any = [];
+			
+			for(let key in this.parents)
+			{
+				if(this.parents[key] && this.parents[key].includes(currentPerson))
+					result.push(key);
+			}
+			
+			el.createEl("p", { text: "Kinder: " + String(result).valueOf() });
 		});
   	}
 	
 	async recalculateAncestry()
 	{
+		this.parents = Array();
+		
 		const { vault } = this.app;
+		const files = vault.getMarkdownFiles()
 		
-		const fileContents: string[] = await Promise.all(
-      		vault.getMarkdownFiles().map((file) => vault.cachedRead(file))
-    	);
+		for (let i = 0; i < files.length; i++) 
+		{
+			let content = await vault.cachedRead(files[i]);
+			let match = this.match(content);
+			
+			this.parents[files[i].basename] = match;
+		}
+	}
+	
+	match(content: string)
+	{
+		const regex = /Eltern: (.*)/g
 		
-		const regex = 'Eltern: \[\[([^]]+)]](?>[, ]+\[\[([^]]+)]])?';
-		let parents : RegExpMatchArray | null = null;
+		let regexResult = content.match(regex);
 		
-		fileContents.forEach((content) => {
-			parents = content.match(regex);
-		});
+		if(!regexResult)
+			return
+			
+		let match = regexResult[0];
 		
-		console.log(parents);
-	}	
+		match = match.substring(8);
+		
+		let result = [];
+		
+		let in_bracket = false;
+		
+		let match_start = 0;
+		let match_end = 0;
+		
+		for (var i = 0; i < match.length; i++) 
+		{
+			if(!in_bracket)
+			{
+				if(match.charAt(i) == "[" && match.charAt(i+1) == "[")
+				{
+					in_bracket = true;
+					match_start = i+2;
+				}
+			}
+			else
+			{
+				if(match.charAt(i) == "]" && match.charAt(i+1) == "]")
+				{
+					in_bracket = false;
+					match_end = i;
+					
+					result.push(match.substring(match_start, match_end));
+				}
+				
+			}
+		}
+		
+		return result;
+	}
 	
   	async onunload() 
 	{
-    	console.log('Unloading ancestry')
+    	console.log('Unloading ancestry');
   	}
 }
